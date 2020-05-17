5d0a88f5419b546e3ac141e89d19db29df077a6c
Suvink <hello@suvink.me>
2020-05-17 11:12:54 +0530

    Moved API keys into secrets

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,14 @@
 import { initializeApp } from 'firebase'
 
 const app = initializeApp({
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: "furgetmenot-b0e6d.firebaseapp.com",
-    databaseURL: "https://furgetmenot-b0e6d.firebaseio.com",
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    storageBucket: "furgetmenot-b0e6d.appspot.com",
-    messagingSenderId: process.env.FIREBASE_MSG_ID,
-    appId: process.env.FIREBASE_APPID,
-    measurementId: process.env.FIREBASE_MES_ID
+    apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
+    authDomain: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.VUE_APP_FIREBASE_DBURL,
+    projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.VUE_APP_FIREBASE_STORAGE_BKT,
+    messagingSenderId: process.env.VUE_APP_FIREBASE_MSG_ID,
+    appId: process.env.VUE_APP_FIREBASE_APPID,
+    measurementId: process.env.VUE_APP_FIREBASE_MES_ID
 })
 
 export const db = app.database()
